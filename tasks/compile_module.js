4e297b1384bedca9ead141d1c9a5d202b884a9f3
Rick Ekelschot <rick@frontmen.nl>
2015-06-30 11:55:41 +0200

    Copy files when module is compileable and improved logging

--- tasks/compile_module.js
+++ tasks/compile_module.js
@@ -16,6 +16,7 @@ module.exports = function (grunt) {
     'use strict';
 
     grunt.task.loadNpmTasks('grunt-bower-postinst');
+    grunt.task.loadNpmTasks('grunt-contrib-copy');
 
     grunt.registerMultiTask('compile_module', '', function () {
         var options = this.options(),
@@ -28,19 +29,20 @@ module.exports = function (grunt) {
 
         var checkModule = function (module, moduleName) {
             grunt.log.writeln(' ');
-            grunt.log.writeln('Check: ' + moduleName);
-            var modulePath = stripTrailingSlash(module.path);
+            grunt.log.subhead(moduleName);
+            module.path = stripTrailingSlash(module.path);
 
-            if (moduleExists(modulePath)) {
-                if (moduleInstalled(modulePath)) {
+            if (moduleExists(module.path)) {
+                if (moduleInstalled(module.path)) {
                     compileModule(module, moduleName);
                 } else {
-                    grunt.log.writeln('Not compilable, download distribution.');
-                    downloadModule(modulePath, moduleName);
+                    grunt.log.writeln(options.file + ' file not found, module is not compilable.');
+                    grunt.log.ok('Download distribution.')
+                    downloadModule(module.path, moduleName);
                 }
             } else {
-                grunt.log.writeln('Module does not exist, skipping..');
-                done();
+                var error = grunt.util.error('Module does not exist in your bower_components directory. Have you tried to run a bower update?');
+                throw error;
             }
         };
 
@@ -63,49 +65,109 @@ module.exports = function (grunt) {
                 }
             };
 
-            grunt.log.writeln('Compiling..');
 
+
+            grunt.log.ok('Compile');
             grunt.config.set('bower_postinst', postInstConfig);
-            grunt.task.run('bower_postinst:' + moduleName);
+
+            if (setCopyConfig(module, moduleName)) {
+                grunt.task.run('bower_postinst:' + moduleName, 'copy:compile_module-' + moduleName);
+            } else {
+                grunt.task.run('bower_postinst:' + moduleName);
+            }
 
             done();
+
+        };
+
+        var setCopyConfig = function (module, moduleName) {
+            var files = getFiles(module.path),
+                copyConfig = {},
+                copyFiles = [];
+
+            if (files.length > 0) {
+                _.each(files, function (file) {
+                    var filePath = module.path + '/' + file,
+                        pattern = /[\*]{1,2}[.a-z]{0,3}/;
+
+                    if (filePath.match(pattern)) {
+                        copyFiles.push({
+                            cwd: filePath.replace(pattern, ''),
+                            src: filePath.match(pattern)[0],
+                            dest: file.replace(pattern, ''),
+                            flatten: false,
+                            filter: 'isFile',
+                            mode: true,
+                            expand: true
+                        });
+                    } else {
+                        copyFiles.push({
+                            src: filePath,
+                            dest: file
+                        });
+                    }
+
+                });
+
+                copyConfig['compile_module-' + moduleName] = {
+                    files: copyFiles
+                };
+
+                grunt.config.set('copy', _.extend(grunt.config.get('copy'), copyConfig));
+                return true;
+            }
+
+            return false;
         };
 
         var downloadModule = function (modulePath, moduleName) {
-            var filePath = modulePath + '/' + 'bower.json';
+            var files = getFiles(modulePath);
+
+            if (files.length > 0) {
+                numDownloads[moduleName] = files.length;
+
+                files.forEach(function (file) {
+                    downloadFile(file, moduleName);
+                });
+            } else {
+                downloadComplete(moduleName);
+            }
+        };
+
+        var getFiles = function (modulePath) {
+            var filePath = modulePath + '/' + 'bower.json',
+                files = [];
 
             if (grunt.file.exists(filePath)) {
-                var json = grunt.file.readJSON(filePath),
-                    files;
+                var json = grunt.file.readJSON(filePath);
 
                 if (_.has(json, 'main')) {
                     files = json.main;
                     if (!_.isArray(files)) {
                         files = [files];
                     }
-
-                    numDownloads[moduleName] = files.length;
-
-                    files.forEach(function (file) {
-                        downloadFile(file, moduleName);
-                    });
                 } else {
-                    grunt.log.writeln('No compiled source path specified in package.json, skipping');
-                    downloadComplete(moduleName);
+                    grunt.log.writeln('No main specified in bower.json!');
                 }
             }
+
+            return files;
+        };
+
+        var createDirectory = function (dir) {
+            grunt.file.mkdir(dir);
         };
 
         var downloadFile = function (filePath, moduleName) {
 
             var path = stripTrailingSlash(options.sourceServer) + '/' + filePath;
 
-            grunt.log.writeln('Downloading (' + path + ')');
-            http.get(path, function(response) {
+            grunt.verbose.writeln('Downloading (' + path + ')');
+            http.get(path, function (response) {
                 if (response.statusCode === 200) {
                     var targetDir = stripTrailingSlash(options.targetDir);
-                    grunt.file.mkdir(targetDir);
-                    grunt.file.mkdir(getFilePath(targetDir + '/' + filePath));
+                    createDirectory(targetDir);
+                    createDirectory(getFilePath(targetDir + '/' + filePath));
 
                     var file = fs.createWriteStream(targetDir + '/' + filePath, {
                         flags: 'w',
@@ -117,8 +179,8 @@ module.exports = function (grunt) {
                         file.close(downloadComplete.bind(this, moduleName));  // close() is async, call cb after close completes.
                     });
                 } else {
-                    grunt.log.writeln('error (' + path + ')');
-                    downloadComplete(moduleName);
+                    var error = grunt.util.error('File not found (' + path + ')');
+                    throw error;
                 }
             });
         };
